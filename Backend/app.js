6c3c54e809f47d2cf68c0c2af93236c65d4a6f39
DeyanAzSum <deyan662@gmail.com>
2025-03-20 22:00:24 +0200

    user vmesto email

--- Backend/app.js
+++ Backend/app.js
@@ -148,7 +148,7 @@ const transporter = nodemailer.createTransport({
     port: 587,
     secure: false,
     auth: {
-      user: process.env.EMAIL,
+      user: process.env.USER,
       pass: process.env.APP_PASS,
     },
   });
