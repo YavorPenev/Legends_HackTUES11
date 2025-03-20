412f7bb3d5eee3818fe790fb30d09afb19c18791
YavorPenev <yavorpen@gmail.com>
2025-03-21 00:17:43 +0200

    fix apps,js

--- Backend/app.js
+++ Backend/app.js
@@ -17,11 +17,13 @@ const router = express.Router();
 
 // MySQL Database Connection
 const db = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "efedrin12",
-    database: "legendsdb"
-});
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    port: process.env.DB_PORT
+  });
+
 
 db.connect((err) => {
     if (err) throw err;
