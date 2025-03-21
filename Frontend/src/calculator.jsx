1abcc3b5b548533c5aa9a472e9e18ca44c91d1c6
KristianVelinov <kristian.velinov@icloud.com>
2025-03-21 18:50:48 +0200

    actually did something- the react is now working at localhost! (no css)

--- Frontend/src/calculator.jsx
+++ Frontend/src/calculator.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router";
 import "./styles/calculator.css";
 
 function Calc(loan, interest, months) {
